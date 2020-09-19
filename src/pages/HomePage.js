b14112d6a69425f9e5bdffb1cf1a087b49937b56
Colman Currie <curriecolman@gmail.com>
2020-09-19 15:36:16 -0700

    move the sign up from App.js to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 const HomePage = () => (
     <>
     <h1>Portland Food Blog</h1>
     <img style={{maxWidth: "100%"}} src= "https://pbs.twimg.com/media/DY2bN3VWkAYBdXC.jpg" alt="gathering"/>
+    <AmplifySignOut />
+
     </>
     
 );
 
-export default HomePage;
+export default withAuthenticator(HomePage);
